e1bfda5d197c645f502b5f3a95a1424fdacba33f
rmitsubayashi <ryomitsu@gmail.com>
2018-12-19 20:42:49 +0900

    revised mock so we need to scroll

--- app/src/main/java/com/lifegoaltracker/repository/goal/GoalRepository.kt
+++ app/src/main/java/com/lifegoaltracker/repository/goal/GoalRepository.kt
@@ -36,7 +36,71 @@ class GoalRepository @Inject constructor(private val goalDao: GoalDao) {
                 GoalProperties(0, ID(0)),
                 GoalStatus()
         )
-        val goalList = listOf(goal1, goal2)
+        val goal3 = Goal(ID(1),
+                GoalUserFields("Big 6 workout six times a week",
+                        "I DON'T want to be a scrawny engineer,",
+                        DueDate(Date(Year(2018), Month.DECEMBER, WeekOfMonth.WEEK_FOUR), GoalSpan.THREE_MONTHS)
+                ),
+                GoalProperties(0, ID(0)),
+                GoalStatus()
+        )
+        val goal4 = Goal(ID(1),
+                GoalUserFields("Big 6 workout six times a week",
+                        "I DON'T want to be a scrawny engineer,",
+                        DueDate(Date(Year(2018), Month.DECEMBER, WeekOfMonth.WEEK_FOUR), GoalSpan.THREE_MONTHS)
+                ),
+                GoalProperties(0, ID(0)),
+                GoalStatus()
+        )
+        val goal5 = Goal(ID(1),
+                GoalUserFields("Big 6 workout six times a week",
+                        "I DON'T want to be a scrawny engineer,",
+                        DueDate(Date(Year(2018), Month.DECEMBER, WeekOfMonth.WEEK_FOUR), GoalSpan.THREE_MONTHS)
+                ),
+                GoalProperties(0, ID(0)),
+                GoalStatus()
+        )
+        val goal6 = Goal(ID(1),
+                GoalUserFields("Big 6 workout six times a week",
+                        "I DON'T want to be a scrawny engineer,",
+                        DueDate(Date(Year(2018), Month.DECEMBER, WeekOfMonth.WEEK_FOUR), GoalSpan.THREE_MONTHS)
+                ),
+                GoalProperties(0, ID(0)),
+                GoalStatus()
+        )
+        val goal7 = Goal(ID(1),
+                GoalUserFields("Big 6 workout six times a week",
+                        "I DON'T want to be a scrawny engineer,",
+                        DueDate(Date(Year(2018), Month.DECEMBER, WeekOfMonth.WEEK_FOUR), GoalSpan.THREE_MONTHS)
+                ),
+                GoalProperties(0, ID(0)),
+                GoalStatus()
+        )
+        val goal8 = Goal(ID(1),
+                GoalUserFields("Big 6 workout six times a week",
+                        "I DON'T want to be a scrawny engineer,",
+                        DueDate(Date(Year(2018), Month.DECEMBER, WeekOfMonth.WEEK_FOUR), GoalSpan.THREE_MONTHS)
+                ),
+                GoalProperties(0, ID(0)),
+                GoalStatus()
+        )
+        val goal9 = Goal(ID(1),
+                GoalUserFields("Big 6 workout six times a week",
+                        "I DON'T want to be a scrawny engineer,",
+                        DueDate(Date(Year(2018), Month.DECEMBER, WeekOfMonth.WEEK_FOUR), GoalSpan.THREE_MONTHS)
+                ),
+                GoalProperties(0, ID(0)),
+                GoalStatus()
+        )
+        val goal10 = Goal(ID(1),
+                GoalUserFields("Big 6 workout six times a week",
+                        "I DON'T want to be a scrawny engineer,",
+                        DueDate(Date(Year(2018), Month.DECEMBER, WeekOfMonth.WEEK_FOUR), GoalSpan.THREE_MONTHS)
+                ),
+                GoalProperties(0, ID(0)),
+                GoalStatus()
+        )
+        val goalList = listOf(goal1, goal2, goal3, goal4, goal5, goal6, goal7, goal8, goal9, goal10)
         return MutableLiveData<List<Goal>>().apply{value=goalList}
 
         /* return goalDao.queryGoals(queryParameters.startDate,
@@ -49,7 +113,7 @@ class GoalRepository @Inject constructor(private val goalDao: GoalDao) {
                         "I want to be fit enough to REALLY enjoy playing soccer",
                         DueDate(Date(Year(2018), Month.DECEMBER, WeekOfMonth.WEEK_FOUR), GoalSpan.ONE_MONTH)
                 ),
-                GoalProperties(0, visionID),
+                GoalProperties(0, ID(0)),
                 GoalStatus()
         )
         val goal2 = Goal(ID(1),
@@ -57,10 +121,98 @@ class GoalRepository @Inject constructor(private val goalDao: GoalDao) {
                         "I DON'T want to be a scrawny engineer,",
                         DueDate(Date(Year(2018), Month.DECEMBER, WeekOfMonth.WEEK_FOUR), GoalSpan.THREE_MONTHS)
                 ),
-                GoalProperties(0, visionID),
+                GoalProperties(0, ID(0)),
                 GoalStatus()
         )
-        val goalList = listOf(goal1, goal2)
+        val goal3 = Goal(ID(1),
+                GoalUserFields("Big 6 workout six times a week",
+                        "I DON'T want to be a scrawny engineer,",
+                        DueDate(Date(Year(2018), Month.DECEMBER, WeekOfMonth.WEEK_FOUR), GoalSpan.THREE_MONTHS)
+                ),
+                GoalProperties(0, ID(0)),
+                GoalStatus()
+        )
+        val goal4 = Goal(ID(1),
+                GoalUserFields("Big 6 workout six times a week",
+                        "I DON'T want to be a scrawny engineer,",
+                        DueDate(Date(Year(2018), Month.DECEMBER, WeekOfMonth.WEEK_FOUR), GoalSpan.THREE_MONTHS)
+                ),
+                GoalProperties(0, ID(0)),
+                GoalStatus()
+        )
+        val goal5 = Goal(ID(1),
+                GoalUserFields("Big 6 workout six times a week",
+                        "I DON'T want to be a scrawny engineer,",
+                        DueDate(Date(Year(2018), Month.DECEMBER, WeekOfMonth.WEEK_FOUR), GoalSpan.THREE_MONTHS)
+                ),
+                GoalProperties(0, ID(0)),
+                GoalStatus()
+        )
+        val goal6 = Goal(ID(1),
+                GoalUserFields("Big 6 workout six times a week",
+                        "I DON'T want to be a scrawny engineer,",
+                        DueDate(Date(Year(2018), Month.DECEMBER, WeekOfMonth.WEEK_FOUR), GoalSpan.THREE_MONTHS)
+                ),
+                GoalProperties(0, ID(0)),
+                GoalStatus()
+        )
+        val goal7 = Goal(ID(1),
+                GoalUserFields("Big 6 workout six times a week",
+                        "I DON'T want to be a scrawny engineer,",
+                        DueDate(Date(Year(2018), Month.DECEMBER, WeekOfMonth.WEEK_FOUR), GoalSpan.THREE_MONTHS)
+                ),
+                GoalProperties(0, ID(0)),
+                GoalStatus()
+        )
+        val goal8 = Goal(ID(1),
+                GoalUserFields("Big 6 workout six times a week",
+                        "I DON'T want to be a scrawny engineer,",
+                        DueDate(Date(Year(2018), Month.DECEMBER, WeekOfMonth.WEEK_FOUR), GoalSpan.THREE_MONTHS)
+                ),
+                GoalProperties(0, ID(0)),
+                GoalStatus()
+        )
+        val goal9 = Goal(ID(1),
+                GoalUserFields("Big 6 workout six times a week",
+                        "I DON'T want to be a scrawny engineer,",
+                        DueDate(Date(Year(2018), Month.DECEMBER, WeekOfMonth.WEEK_FOUR), GoalSpan.THREE_MONTHS)
+                ),
+                GoalProperties(0, ID(0)),
+                GoalStatus()
+        )
+        val goal10 = Goal(ID(1),
+                GoalUserFields("Big 6 workout six times a week",
+                        "I DON'T want to be a scrawny engineer,",
+                        DueDate(Date(Year(2018), Month.DECEMBER, WeekOfMonth.WEEK_FOUR), GoalSpan.THREE_MONTHS)
+                ),
+                GoalProperties(0, ID(0)),
+                GoalStatus()
+        )
+        val goal11 = Goal(ID(1),
+                GoalUserFields("Big 6 workout six times a week",
+                        "I DON'T want to be a scrawny engineer,",
+                        DueDate(Date(Year(2018), Month.DECEMBER, WeekOfMonth.WEEK_FOUR), GoalSpan.THREE_MONTHS)
+                ),
+                GoalProperties(0, ID(0)),
+                GoalStatus()
+        )
+        val goal12 = Goal(ID(1),
+                GoalUserFields("Big 6 workout six times a week",
+                        "I DON'T want to be a scrawny engineer,",
+                        DueDate(Date(Year(2018), Month.DECEMBER, WeekOfMonth.WEEK_FOUR), GoalSpan.THREE_MONTHS)
+                ),
+                GoalProperties(0, ID(0)),
+                GoalStatus()
+        )
+        val goal13 = Goal(ID(1),
+                GoalUserFields("Big 6 workout six times a week",
+                        "I DON'T want to be a scrawny engineer,",
+                        DueDate(Date(Year(2018), Month.DECEMBER, WeekOfMonth.WEEK_FOUR), GoalSpan.THREE_MONTHS)
+                ),
+                GoalProperties(0, ID(0)),
+                GoalStatus()
+        )
+        val goalList = listOf(goal1, goal2, goal3, goal4, goal5, goal6, goal7, goal8, goal9, goal10, goal11, goal12, goal13)
         return MutableLiveData<List<Goal>>().apply{value=goalList}
 
         /* return goalDao.queryVisionGoals(queryParameters.startDate,

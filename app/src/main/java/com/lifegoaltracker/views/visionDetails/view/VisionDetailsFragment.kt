7345c6da4b9d000ba9497623cea2e8fd43cc3e0d
rmitsubayashi <ryomitsu@gmail.com>
2018-12-24 11:46:17 +0900

    added edit vision

--- app/src/main/java/com/lifegoaltracker/views/visionDetails/view/VisionDetailsFragment.kt
+++ app/src/main/java/com/lifegoaltracker/views/visionDetails/view/VisionDetailsFragment.kt
@@ -10,9 +10,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.lifegoaltracker.R
 import com.lifegoaltracker.di.Injectable
+import com.lifegoaltracker.model.vision.Vision
 import com.lifegoaltracker.repository.ID
+import com.lifegoaltracker.views.addEditVision.view.AddEditVisionFragment
 import com.lifegoaltracker.views.visionDetails.viewmodel.VisionDetailsViewModel
 import kotlinx.android.synthetic.main.fragment_vision_details.*
 import javax.inject.Inject
@@ -34,10 +37,21 @@ class VisionDetailsFragment: Fragment(), Injectable {
         val visionID = arguments?.getSerializable("visionID") as ID
         viewModel.fetchVision(visionID)
         viewModel.vision?.observe(this, Observer {
-            textview_vision_details_title.text = it?.userFields?.title
-            textview_vision_details_description.text = it?.userFields?.description
-            textview_vision_details_reason.text = it?.userFields?.reason
+            it?.let {
+                vision ->
+                textview_vision_details_title.text = vision.userFields.title
+                textview_vision_details_description.text = vision.userFields.description
+                textview_vision_details_reason.text = vision.userFields.reason
+                setEditVisionListener(vision)
+            }
         })
+    }
+
+    private fun setEditVisionListener(vision: Vision) {
+        button_vision_details_edit.setOnClickListener {
+            val bundle = AddEditVisionFragment.createBundle(vision)
+            it.findNavController().navigate(R.id.action_mainViewPagerFragment_to_addEditVisionFragment, bundle)
+        }
     }
 
     companion object {
